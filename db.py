7030a6355f4190761e2374d41a6ca2accda4a075
musor-drop123 <dannialvaetori@gmail.com>
2026-09-18 05:57:20 +0000

    update

--- db.py
+++ db.py
@@ -49,4 +49,4 @@ async def get_tasks_by_number(db: Connection, number):
 async def create_user(db: Connection, id,name ,surname ,password ,is_admin ):
     await db.execute("""INSERT INTO users VALUES ($1, $2, $3, $4, $5);""", id,name ,surname ,password ,is_admin )
 async def create_task(db: Connection, id, name, number, diff, text, answer, description):
-    await db.execute("""INSERT INTO tasks VALUES ($1, $2, $3, $4, $5, 6$, $7)""", id, name, number, diff, text, answer, description)
+    await db.execute("""INSERT INTO tasks VALUES ($1, $2, $3, $4, $5, $6, $7)""", id, name, number, diff, text, answer, description)
